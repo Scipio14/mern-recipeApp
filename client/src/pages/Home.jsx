d5c10c011627228be504f94f44c4ffb3ff46d162
Federico Macías <fma.abogado@gmail.com>
2023-03-24 13:40:11 -0600

    Loading recipes to the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,54 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserId.js";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [recipes, setRecipes] = useState([]);
+  const userID = useGetUserID();
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/recipes");
+        setRecipes(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchRecipe();
+  }, []);
+
+  const saveRecipe = async (recipeID) => {
+    try {
+      const response = await axios.put("http://localhost:5000/recipes", {
+        recipeID,
+        userID,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              <button onClick={() => saveRecipe(recipe._id)}>Save</button>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime}min</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default Home;
